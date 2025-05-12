8ae66f55e3c505e193de230c61911bfabc84dd8f
moti7846 <8563816@gmail.com>
2025-05-12 17:20:49 +0300

    add Functions

--- my_first_project_in_c#/Program.cs
+++ my_first_project_in_c#/Program.cs
@@ -8,8 +8,67 @@ namespace my_first_project_in_c_
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void menu()
         {
+            return;
+        }
+        static void input_validation()
+        {
+            return;
+        }
+        static void selection_validation()
+        {
+            return;
+        }
+        static void printer()
+        {
+            return;
+        }
+        static void input_deoding()
+        {
+            return;
+        }
+        static void args_reverse()
+        {
+            return;
+        }
+        static void sorting()
+        {
+            return;
+        }
+        static void maximum()
+        {
+            return;
+        }
+        static void minimum()
+        {
+            return;
+        }
+        static void average()
+        {
+            return;
+        }
+        static void list_length()
+        {
+            return;
+        }
+        static void total()
+        {
+            return;
+        }
+        static void exit()
+        {
+            return;
+        }
+        static void nul()
+        {
+            return;
+        }
+
+
+        static void Main(int[] args)
+        {
+
         }
     }
 }
